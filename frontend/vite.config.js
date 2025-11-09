8b19ab6fded6754bbde37b6fc624e9fc7842cc9e
ritesh harge <ritesh@example.com>
2025-11-09 12:55:20 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/AmazonProductListingWithAI/",  
+  base: "/OptimizeList-AI/", // match your repo name
   plugins: [react()],
   server: {
     proxy: {
